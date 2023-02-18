5ce03bf8c7e1157fffa219850e76d769c11f0b30
Maryprincess1 <123330292+Maryprincess1@users.noreply.github.com>
2023-02-18 22:19:33 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
+
 /**
- *  *main- A program that uses puts to print a string
- *   *Return: ends with 0
- *    */
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
