20c4122d7a50ec894112f03578d11c6555557bfa
Maryprincess1 <omeniumehmaryprincess@gmail.com>
2023-02-18 16:06:33 -0800

    New commit she

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-#include <stdio.h>
-/**
- *    * main - Prints onto screen
- *       *
- *          * Return: zero
- *             */
-int main(void)
-{
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
-}
-
-=======
 #include <stdio.h>
 /**
    * main - Prints onto screen
@@ -23,4 +9,4 @@ int main(void)
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
->>>>>>> e816d2df92e0defdb552f73c976b97fdf2cac434
+
