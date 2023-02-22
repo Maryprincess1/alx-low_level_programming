f97b09e9175f466b782f0513f9cf6052c59fa70f
Maryprincess1 <123330292+Maryprincess1@users.noreply.github.com>
2023-02-22 21:06:54 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,25 +1,20 @@
-#!/bin/bash
 #include "main.h"
-
 /**
- *  * print_alphabet_x10 - print alphabet
- *   *
- *    * Return: Always 0.
- *     */
-void print_alphabet_x10(void)
+ *main - Entry point
+ *
+ *Return: nothing, return void
+ */
+int main(void)
 {
-		int alphabet;
-			int count;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-				count = 0;
-					while (count < 10)
-							{
-										for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
-													{
-																	_putchar(alphabet);
-																			}
-
-												count++;
-														_putchar('\n');
-															}
+	return (0);
 }
