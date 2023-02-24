ec930277e5941b9d3e89c4c9a3ae3b4651216c1c
Maryprincess1 <123330292+Maryprincess1@users.noreply.github.com>
2023-02-24 10:44:23 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,39 +1,15 @@
-#include <stdio.h>
-#include "math.h"
+#include "main.h"
 
 /**
- * main - finds and prints the largest prime factor of the number 612852475143
- * followed by a new line
- * Return: Always 0 (Success)
+ * _isupper - checks for uppercase character
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
  */
-int main(void)
+int _isupper(int c)
 {
-	long int n;
-	long int max;
-	long int i;
-
-	n = 612852475143;
-	max = -1;
-
-	while (n % 2 == 0)
+	if (c >= 'A' && c <= 'Z')
 	{
-		max = 2;
-		n /= 2;
+		return (1);
 	}
-
-	for (i = 3; i <= sqrt(n); i = i + 2)
-	{
-		while (n % i == 0)
-		{
-			max = i;
-			n = n / i;
-		}
-	}
-
-	if (n > 2)
-		max = n;
-
-	printf("%ld\n", max);
-
 	return (0);
 }
